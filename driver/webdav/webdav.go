4386cd853d57a4d417c5dfbcb6f92a38fe2357b5
Evgeni Golov <evgeni@golov.de>
2021-12-22 21:28:47 +0100

    add webdav driver

---
+++ driver/webdav/webdav.go
@@ -0,0 +1,92 @@
+package webdav
+
+import (
+  "io"
+  "os"
+  "strings"
+
+	"github.com/studio-b12/gowebdav"
+  "goftp.io/server/v2"
+)
+
+type Driver struct {
+  client *gowebdav.Client
+}
+
+func NewDriver(url string, user string, password string, useSSL bool) (server.Driver, error) {
+  c := gowebdav.NewClient(url, user, password)
+  return &Driver{
+    client: c,
+  }, nil
+}
+
+func buildWebDAVPath(p string) string {
+	return strings.TrimPrefix(p, "/")
+}
+
+func (driver *Driver) DeleteDir(ctx *server.Context, path string) error {
+  p := buildWebDAVPath(path)
+	return driver.client.Remove(p)
+}
+
+func (driver *Driver) DeleteFile(ctx *server.Context, path string) error {
+	return driver.client.Remove(buildWebDAVPath(path))
+}
+
+func (driver *Driver) GetFile(ctx *server.Context, path string, offset int64) (int64, io.ReadCloser, error) {
+  p := buildWebDAVPath(path)
+  object, err := driver.client.ReadStream(p)
+  if err != nil {
+		return 0, nil, err
+	}
+	defer func() {
+		if err != nil && object != nil {
+			object.Close()
+		}
+	}()
+	info, err := driver.client.Stat(p)
+	if err != nil {
+		return 0, nil, err
+	}
+
+	return info.Size() - offset, object, nil
+}
+
+func (driver *Driver) ListDir(ctx *server.Context, path string, callback func(os.FileInfo) error) error {
+  p := buildWebDAVPath(path)
+  if p == "/" {
+		p = ""
+	}
+  objects, err := driver.client.ReadDir(p)
+  if err != nil {
+		return err
+	}
+  for _, object := range objects {
+    err := callback(object)
+		if err != nil {
+		  return err
+		}
+  }
+  return nil
+}
+
+func (driver *Driver) MakeDir(ctx *server.Context, path string) error {
+  return driver.client.Mkdir(path, os.ModePerm)
+}
+
+func (driver *Driver) PutFile(ctx *server.Context, destPath string, data io.Reader, offset int64) (int64, error) {
+  p := buildWebDAVPath(destPath)
+  err := driver.client.WriteStream(p, data, 0644)
+  return 0, err
+}
+
+func (driver *Driver) Rename(ctx *server.Context, fromPath string, toPath string) error {
+  fp := buildWebDAVPath(fromPath)
+  tp := buildWebDAVPath(toPath)
+  return driver.client.Rename(fp, tp, false)
+}
+
+func (driver *Driver) Stat(ctx *server.Context, path string) (os.FileInfo, error) {
+  p := buildWebDAVPath(path)
+  return driver.client.Stat(p)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/evgeni/ftpdav
 
 go 1.16
+
+require (
+	github.com/jlaffaye/ftp v0.0.0-20190624084859-c1312a7102bf
+	github.com/stretchr/testify v1.3.0
+	github.com/studio-b12/gowebdav v0.0.0-20211109083228-3f8721cd4b6f
+	goftp.io/server/v2 v2.0.0
+)
